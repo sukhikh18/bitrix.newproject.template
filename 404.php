531868beff5229208de0f3e53d70ea0d24237ef3
nikolays93 <nikolays93@ya.ru>
2019-06-12 19:36:48 +0400

    set 404 like at wordpress

--- 404.php
+++ 404.php
@@ -1,20 +1,24 @@
 <?
 include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
 
-CHTTP::SetStatus("404 Not Found");
-@define("ERROR_404","Y");
+if (!defined("ERROR_404")) define("ERROR_404", "Y");
+\CHTTP::setStatus("404 Not Found");
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-$APPLICATION->SetTitle("404 Not Found");
+$APPLICATION->SetTitle("Страница не найдена");
 
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"36000000"
-	)
-);
+?>
+<div class="container">
+    <main id="main" class="404 content" role="main">
+        <article class="error-404 not-found">
+            <h1><?$APPLICATION->ShowTitle(false);?></h1>
+            <div class="error-content entry-content">
+                <p>К сожалению эта страница не найдена или не доступна. Попробуйте зайти позднее или воспользуйтесь главным меню для перехода по основным страницам.</p>
+            </div><!-- .entry-content -->
+        </article><!-- .error-404 -->
+    </main><!-- #main -->
+</div><!-- .container -->
+<?php
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
