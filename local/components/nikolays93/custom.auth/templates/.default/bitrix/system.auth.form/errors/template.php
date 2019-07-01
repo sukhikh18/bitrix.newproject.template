1092c7c57b0871e2185dd318b4f60ed0d3909a15
nikolays93 <nikolays93@ya.ru>
2019-07-01 10:50:29 +0400

    clear components

--- local/components/nikolays93/custom.auth/templates/.default/bitrix/system.auth.form/errors/template.php
+++
@@ -1,4 +0,0 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)	die();
-
-if($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
-	ShowMessage($arResult['ERROR_MESSAGE']);
--- local/components/nikolays93/html.bootstrap.collapses/lang/ru/.parameters.php
+++
@@ -1,3 +0,0 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$MESS["SHOW_TIP"] = 'Порядковый номер блока который нужно показать при загрузке страницы. 0 - все скрыты.';
