32a9c897b3a8bbdd19763a4eb34f3a34c7da54fa
nikolays93 <nikolays93@ya.ru>
2019-11-28 11:51:50 +0400

    Upgrade catalog component template by default.

--- local/templates/new.project/components/bitrix/catalog/.default/section.php
+++ local/templates/new.project/components/bitrix/catalog/.default/section.php
@@ -1,6 +1,4 @@
-<? if ( ! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
-    die();
-}
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -11,42 +9,71 @@
 /** @var string $templateFile */
 /** @var string $templateFolder */
 /** @var string $componentPath */
-
 /** @var CBitrixComponent $component */
-
-use Bitrix\Main;
 use Bitrix\Main\Loader;
 use Bitrix\Main\ModuleManager;
 
 $this->setFrameMode(true);
 
-Conditions::set_location('catalog', true);
+if (!isset($arParams['FILTER_VIEW_MODE']) || (string)$arParams['FILTER_VIEW_MODE'] == '')
+	$arParams['FILTER_VIEW_MODE'] = 'VERTICAL';
+$arParams['USE_FILTER'] = (isset($arParams['USE_FILTER']) && $arParams['USE_FILTER'] == 'Y' ? 'Y' : 'N');
 
-$documentRoot = Main\Application::getDocumentRoot();
-$folder       = $this->GetFolder();
+$isVerticalFilter = ('Y' == $arParams['USE_FILTER'] && $arParams["FILTER_VIEW_MODE"] == "VERTICAL");
+$isSidebar = ($arParams["SIDEBAR_SECTION_SHOW"] == "Y" && isset($arParams["SIDEBAR_PATH"]) && !empty($arParams["SIDEBAR_PATH"]));
+$isFilter = ($arParams['USE_FILTER'] == 'Y');
 
-// Sidebar
-$isSidebar = ($arParams["SIDEBAR_SECTION_SHOW"] == "Y"
-              && isset($arParams["SIDEBAR_PATH"]) && ! empty($arParams["SIDEBAR_PATH"]));
+if ($isFilter)
+{
+	$arFilter = array(
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+		"ACTIVE" => "Y",
+		"GLOBAL_ACTIVE" => "Y",
+	);
+	if (0 < intval($arResult["VARIABLES"]["SECTION_ID"]))
+		$arFilter["ID"] = $arResult["VARIABLES"]["SECTION_ID"];
+	elseif ('' != $arResult["VARIABLES"]["SECTION_CODE"])
+		$arFilter["=CODE"] = $arResult["VARIABLES"]["SECTION_CODE"];
 
-$file = new Main\IO\File($documentRoot . $folder . "/filter.php");
-if ($file->isExists()) {
-    include($file->getPath());
+	$obCache = new CPHPCache();
+	if ($obCache->InitCache(36000, serialize($arFilter), "/iblock/catalog"))
+	{
+		$arCurSection = $obCache->GetVars();
+	}
+	elseif ($obCache->StartDataCache())
+	{
+		$arCurSection = array();
+		if (Loader::includeModule("iblock"))
+		{
+			$dbRes = CIBlockSection::GetList(array(), $arFilter, false, array("ID"));
+
+			if(defined("BX_COMP_MANAGED_CACHE"))
+			{
+				global $CACHE_MANAGER;
+				$CACHE_MANAGER->StartTagCache("/iblock/catalog");
+
+				if ($arCurSection = $dbRes->Fetch())
+					$CACHE_MANAGER->RegisterTag("iblock_id_".$arParams["IBLOCK_ID"]);
+
+				$CACHE_MANAGER->EndTagCache();
+			}
+			else
+			{
+				if(!$arCurSection = $dbRes->Fetch())
+					$arCurSection = array();
+			}
+		}
+		$obCache->EndDataCache($arCurSection);
+	}
+	if (!isset($arCurSection))
+		$arCurSection = array();
 }
 ?>
-<section class="shop">
-    <div class="row">
-        <div class="sidebar col-2">
-            <? $file = new Main\IO\File($documentRoot . $folder . "/sidebar.php");
-            if ($file->isExists()) {
-                include($file->getPath());
-            } ?>
-        </div>
-        <div class="catalog col-10">
-            <? $file = new Main\IO\File($documentRoot . $folder . "/catalog.php");
-            if ($file->isExists()) {
-                include($file->getPath());
-            } ?>
-        </div>
-    </div>
-</section>
+<div class="row">
+<?
+if ($isVerticalFilter)
+	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_vertical.php");
+else
+	include($_SERVER["DOCUMENT_ROOT"]."/".$this->GetFolder()."/section_horizontal.php");
+?>
+</div>
