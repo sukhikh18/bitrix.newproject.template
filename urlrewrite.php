5f8d8622b096943e5338797a1387ee421aad6d7f
nikolays93 <nikolays93@ya.ru>
2019-12-12 15:26:55 +0400

    Change params.

--- urlrewrite.php
+++ urlrewrite.php
@@ -2,6 +2,22 @@
 $arUrlRewrite=array (
   0 => 
   array (
+    'CONDITION' => '#^\\/?\\/mobileapp/jn\\/(.*)\\/.*#',
+    'RULE' => 'componentName=$1',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/mobileapp/jn.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/rest/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/rest/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
     'CONDITION' => '#^/shop/#',
     'RULE' => '',
     'ID' => 'bitrix:catalog',
