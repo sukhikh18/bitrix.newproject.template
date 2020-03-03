594781e216da7209bce1a0c6e375e21f6011bbee
nikolays93 <nikolays93@ya.ru>
2020-03-03 17:58:24 +0400

    Add sort by selector.

--- local/templates/new.project/components/bitrix/catalog/.default/result_modifier.php
+++ local/templates/new.project/components/bitrix/catalog/.default/result_modifier.php
@@ -1,5 +1,7 @@
 <?php
 
+const SORT_BY_PARAM = 'sort_by';
+
 $arResult['CURRENT_SECTION'] = array(
 	'ID' => 0,
 	'NAME' => 'Каталог'
@@ -13,4 +15,19 @@ if( !empty( $arResult['VARIABLES']['SECTION_ID'] ) ) {
 			"ID" => intval( $arResult['VARIABLES']['SECTION_ID'] ),
 		),
 	))->fetch();
+}
+
+/**
+ * Sort by selector
+ */
+$arResult['SORT_BY'] = ( ! empty( $_REQUEST[SORT_BY_PARAM]) && is_string($_REQUEST[SORT_BY_PARAM]))
+		? $_REQUEST[SORT_BY_PARAM] : false;
+
+if( 'price_asc' === $arResult['SORT_BY'] || 'price_desc' === $arResult['SORT_BY'] ) {
+	$arParams["ELEMENT_SORT_FIELD"] = 'catalog_PRICE_1';
+	$arParams["ELEMENT_SORT_ORDER"] = ( 'price_asc' === $arResult['SORT_BY'] ) ? 'asc' : 'desc';
+}
+elseif( 'stock_desc' === $arResult['SORT_BY'] ) {
+	$arParams["ELEMENT_SORT_FIELD"] = 'CATALOG_QUANTITY';
+	$arParams["ELEMENT_SORT_ORDER"] = 'desc';
 }
