8a9d1562bd779014311a9f467e54ccf5637cb817
nikolays93 <nikolays93@ya.ru>
2020-03-04 17:58:02 +0400

    Fix typo :bug:

--- local/templates/new.project/components/bitrix/catalog/.default/lang/ru/result_modifier.php
+++ local/templates/new.project/components/bitrix/catalog/.default/lang/ru/result_modifier.php
@@ -3,4 +3,4 @@
 $MESS['SORT_BY_LIST_DEFAULT'] = 'По умолчанию';
 $MESS['SORT_BY_LIST_PRICE_ASC'] = 'По цене (сначала дешевле)';
 $MESS['SORT_BY_LIST_PRICE_DESC'] = 'По цене (сначала дороже)';
-$MESS['SORT_BY_LIST_STOCK_DESC'] = 'По наличию';
+$MESS['SORT_BY_LIST_STOCK'] = 'По наличию';
