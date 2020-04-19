bca595b163f7a9e7ca3d56888ba7d24edb463910
nikolays93 <nikolays93@ya.ru>
2020-04-19 17:52:50 +0400

    Fix auth logic

--- urlrewrite.php
+++ urlrewrite.php
@@ -2,13 +2,21 @@
 $arUrlRewrite=array (
   0 => 
   array (
+    'CONDITION' => '#^/auth/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/user/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
     'CONDITION' => '#^\\/?\\/mobileapp/jn\\/(.*)\\/.*#',
     'RULE' => 'componentName=$1',
     'ID' => NULL,
     'PATH' => '/bitrix/services/mobileapp/jn.php',
     'SORT' => 100,
   ),
-  1 => 
+  2 => 
   array (
     'CONDITION' => '#^/rest/#',
     'RULE' => '',
--- user/index.php
+++ user/index.php
@@ -2,40 +2,57 @@
 
 global $USER;
 
-$APPLICATION->SetTitle("Мои заказы");
-
 ?>
+
 <?if(!$USER->IsAuthorized()): ?>
+<div class="row">
+	<div class="col-lg-6">
 	<?$APPLICATION->IncludeComponent(
-	"nikolays93:custom.auth", 
-	".default", 
-	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"PRIVACY_PAGE" => "",
-		"PERSONAL_PAGE" => ""
-	),
-	false
-);?>
+		"seo18:custom.auth",
+		"",
+		Array(
+			"PERSONAL_PAGE" => "",
+			"PRIVACY_PAGE" => ""
+		)
+	);?>
+	</div>
+</div>
+
 <?else:?>
-<?$APPLICATION->IncludeComponent("bitrix:sale.personal.order", "", array(
-	"SEF_MODE" => "Y",
-	"SEF_FOLDER" => "/user/order/",
-	"ORDERS_PER_PAGE" => "10",
-	"PATH_TO_PAYMENT" => PATH_TO_PAYMENT,
-	"PATH_TO_BASKET" => PATH_TO_CART,
-	"SET_TITLE" => "Y",
-	"SAVE_IN_SESSION" => "N",
-	"NAV_TEMPLATE" => "arrows",
-	"SEF_URL_TEMPLATES" => array(
-		"list" => "index.php",
-		"detail" => "detail/#ID#/",
-		"cancel" => "cancel/#ID#/",
-	),
-	"SHOW_ACCOUNT_NUMBER" => "Y",
-	"ALLOW_INNER" => "N",
-	"ONLY_INNER_FULL" => "N"
-	),
-	false
-);?>
+<?$APPLICATION->SetTitle("Мои заказы");?>
+<div class="row">
+	<div class="col-md-8">
+		<?$APPLICATION->IncludeComponent(
+			"bitrix:sale.personal.order",
+			"",
+			Array(
+				"ALLOW_INNER" => "N",
+				"NAV_TEMPLATE" => "arrows",
+				"ONLY_INNER_FULL" => "N",
+				"ORDERS_PER_PAGE" => "10",
+				"PATH_TO_BASKET" => PATH_TO_CART,
+				"PATH_TO_PAYMENT" => PATH_TO_PAYMENT,
+				"SAVE_IN_SESSION" => "N",
+				"SEF_FOLDER" => "/user/order/",
+				"SEF_MODE" => "Y",
+				"SEF_URL_TEMPLATES" => array("list"=>"index.php","detail"=>"detail/#ID#/","cancel"=>"cancel/#ID#/",),
+				"SET_TITLE" => "Y",
+				"SHOW_ACCOUNT_NUMBER" => "Y"
+			)
+		);?>
+	</div>
+	<div class="col-md-4">
+		<?$APPLICATION->IncludeComponent(
+			"bitrix:main.profile",
+			"",
+			Array(
+				"CHECK_RIGHTS" => "Y",
+				"SEND_INFO" => "N",
+				"SET_TITLE" => "N",
+				"USER_PROPERTY" => array()
+			)
+		);?>
+	</div>
+</div>
 <?endif;?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
