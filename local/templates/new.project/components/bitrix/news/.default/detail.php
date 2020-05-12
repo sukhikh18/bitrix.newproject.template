f9025c3f05868b8d72558f5c63452a815e160e23
nikolays93 <nikolays93@ya.ru>
2020-05-12 17:26:47 +0400

    Upgrade complex news component

--- local/templates/new.project/components/bitrix/news/.default/detail.php
+++ local/templates/new.project/components/bitrix/news/.default/detail.php
@@ -17,182 +17,152 @@ use \Bitrix\Main;
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-$assets = Main\Page\Asset::getInstance();
-$min    = ("Y" !== Main\Config\Option::get("main", "use_minified_assets")) ? '' : '.min';
+if(class_exists('HTTP_Query')) {
+    HTTP_Query::get_instance()->set(array(
+        'VARIABLES'   => $arResult["VARIABLES"],
+        "DETAIL_URL"  => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+        "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+        "IBLOCK_URL"  => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
+        "SEARCH_PAGE" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["search"],
+    ));
+}
 
-$path = rtrim('/pages' . $arResult["FOLDER"], '/') . '/detail' . $min . '.css';
-if (file_exists(THEME . $path)) {
-    $assets->addCss(TPL . $path);
+$paramsPrefix = 'DETAIL_';
+$paramsPrefixLen = strlen($paramsPrefix);
+$arParams = array_walk($arParams, function($paramValue, $param) use (&$arParams, $arParamsPrefix, $paramsPrefixLen) {
+    if(0 === strpos($param, $arParamsPrefix)) {
+        // @todo write debug when $arParams[$key] already exists.
+        $arParams[substr($param, $paramsPrefixLen)] = $paramValue;
+        unset($arParams[$param]);
+    }
+});
+
+ob_start();
+$ElementID = $APPLICATION->IncludeComponent(
+    "bitrix:news.detail",
+    '',
+    array_merge($arParams, Array(
+        "DETAIL_URL"        => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+        "SECTION_URL"       => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+        "IBLOCK_URL"        => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
+        "SEARCH_PAGE"       => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["search"],
+        "ELEMENT_ID"        => $arResult["VARIABLES"]["ELEMENT_ID"],
+        "ELEMENT_CODE"      => $arResult["VARIABLES"]["ELEMENT_CODE"],
+        "SECTION_ID"        => $arResult["VARIABLES"]["SECTION_ID"],
+        "SECTION_CODE"      => $arResult["VARIABLES"]["SECTION_CODE"],
+
+        "SET_TITLE"         => "Y",
+        "PAGER_SHOW_ALWAYS" => "N",
+    )),
+    $component
+);
+$bxNewsDetail = ob_get_clean();
+
+if($arParams["USE_CATEGORIES"] == "Y" && $ElementID) {
+    global $arCategoryFilter;
+
+    $obCache    = new CPHPCache;
+    /**
+     * Get unique cache string
+     * @var string $strCacheID
+     */
+    $strCacheID = $componentPath . LANG . $arParams["IBLOCK_ID"] . $ElementID . $arParams["CATEGORY_CODE"];
+    if (($tzOffset = CTimeZone::GetOffset()) <> 0) $strCacheID .= "_" . $tzOffset;
+
+    /**
+     * Get cache live time
+     * @var int $intCacheTime
+     */
+    $isOptionCahceDisabled = COption::GetOptionString("main", "component_cache_on", "Y") == "N";
+    if ($arParams["CACHE_TYPE"] == "N" || $arParams["CACHE_TYPE"] == "A" && $isOptionCahceDisabled) {
+        $intCacheTime = 0;
+    } else {
+        $intCacheTime = intval($arParams["CACHE_TIME"]);
+    }
+
+    /**
+     * To cache filter variables by properties.
+     */
+    if ($obCache->StartDataCache($intCacheTime, $strCacheID, $componentPath)) {
+        $rsProperties = CIBlockElement::GetProperty($arParams["IBLOCK_ID"], $ElementID, "sort", "asc", array(
+            "ACTIVE" => "Y",
+            "CODE" => $arParams["CATEGORY_CODE"]
+        ));
+
+        $arCategoryFilter = array();
+        while ($arProperty = $rsProperties->Fetch()) {
+            if(!empty($arProperty["VALUE"])) {
+                if(is_array($arProperty["VALUE"])) {
+                    foreach ($arProperty["VALUE"] as $value) $arCategoryFilter[$value] = true;
+                } else {
+                    $arCategoryFilter[$arProperty["VALUE"]] = true;
+                }
+            }
+        }
+
+        $obCache->EndDataCache($arCategoryFilter);
+    } else {
+        $arCategoryFilter = $obCache->GetVars();
+    }
+
+    if(!empty($arCategoryFilter)) {
+        $arCategoryFilter = array(
+            "PROPERTY_" . $arParams["CATEGORY_CODE"] => array_keys($arCategoryFilter),
+            "!" . "ID"                               => $ElementID,
+        );
+    }
 }
 
 ?>
-<section class="news news--detail component component--news">
+<section class="news news--detail component--news">
     <section class="news__detail component--news-detail">
-        <? $ElementID = $APPLICATION->IncludeComponent(
-            "bitrix:news.detail",
-            $arParams["DETAIL_TEMPLATE"],
+        <?= $bxNewsDetail ?>
+    </section>
+
+    <? if ($arParams['BACKLINK_TEXT']): $backlinkUrl = $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"]; ?>
+    <p class="news__backlink">
+        <a class="btn btn-primary" href="<?= $backlinkUrl ?>"><?= $arParams['BACKLINK_TEXT'] ?></a>
+    </p>
+    <? endif ?>
+
+    <? if ($arParams["USE_CATEGORIES"] == "Y" && $ElementID && !empty($arCategoryFilter)): ?>
+    <section class="news__categories component--news-list">
+        <h3><?= GetMessage("CATEGORIES") ?></h3>
+
+        <? foreach ($arParams["CATEGORY_IBLOCK"] as $iblock_id): ?>
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            '',
             Array(
-                "DISPLAY_DATE"              => $arParams["DISPLAY_DATE"],
-                "DISPLAY_NAME"              => $arParams["DISPLAY_NAME"],
-                "DISPLAY_PICTURE"           => $arParams["DISPLAY_PICTURE"],
-                "DISPLAY_PREVIEW_TEXT"      => $arParams["DISPLAY_PREVIEW_TEXT"],
-                "IBLOCK_TYPE"               => $arParams["IBLOCK_TYPE"],
-                "IBLOCK_ID"                 => $arParams["IBLOCK_ID"],
-                "FIELD_CODE"                => $arParams["DETAIL_FIELD_CODE"],
-                "PROPERTY_CODE"             => $arParams["DETAIL_PROPERTY_CODE"],
-                "DETAIL_URL"                => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
-                "SECTION_URL"               => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
-                "META_KEYWORDS"             => $arParams["META_KEYWORDS"],
-                "META_DESCRIPTION"          => $arParams["META_DESCRIPTION"],
-                "BROWSER_TITLE"             => $arParams["BROWSER_TITLE"],
-                "SET_CANONICAL_URL"         => $arParams["DETAIL_SET_CANONICAL_URL"],
-                "DISPLAY_PANEL"             => $arParams["DISPLAY_PANEL"],
-                "SET_LAST_MODIFIED"         => $arParams["SET_LAST_MODIFIED"],
-                "SET_TITLE"                 => "Y",
-                "MESSAGE_404"               => $arParams["MESSAGE_404"],
-                "SET_STATUS_404"            => $arParams["SET_STATUS_404"],
-                "SHOW_404"                  => $arParams["SHOW_404"],
-                "FILE_404"                  => $arParams["FILE_404"],
-                "INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-                "ADD_SECTIONS_CHAIN"        => $arParams["ADD_SECTIONS_CHAIN"],
-                "ACTIVE_DATE_FORMAT"        => $arParams["DETAIL_ACTIVE_DATE_FORMAT"],
+                "IBLOCK_ID"                 => $iblock_id,
+                "NEWS_COUNT"                => $arParams["CATEGORY_ITEMS_COUNT"],
+                "SET_TITLE"                 => "N",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
                 "CACHE_TYPE"                => $arParams["CACHE_TYPE"],
                 "CACHE_TIME"                => $arParams["CACHE_TIME"],
                 "CACHE_GROUPS"              => $arParams["CACHE_GROUPS"],
-                "USE_PERMISSIONS"           => $arParams["USE_PERMISSIONS"],
-                "GROUP_PERMISSIONS"         => $arParams["GROUP_PERMISSIONS"],
-                "DISPLAY_TOP_PAGER"         => $arParams["DETAIL_DISPLAY_TOP_PAGER"],
-                "DISPLAY_BOTTOM_PAGER"      => $arParams["DETAIL_DISPLAY_BOTTOM_PAGER"],
-                "PAGER_TITLE"               => $arParams["DETAIL_PAGER_TITLE"],
-                "PAGER_SHOW_ALWAYS"         => "N",
-                "PAGER_TEMPLATE"            => $arParams["DETAIL_PAGER_TEMPLATE"],
-                "PAGER_SHOW_ALL"            => $arParams["DETAIL_PAGER_SHOW_ALL"],
-                "CHECK_DATES"               => $arParams["CHECK_DATES"],
-                "ELEMENT_ID"                => $arResult["VARIABLES"]["ELEMENT_ID"],
-                "ELEMENT_CODE"              => $arResult["VARIABLES"]["ELEMENT_CODE"],
-                "SECTION_ID"                => $arResult["VARIABLES"]["SECTION_ID"],
-                "SECTION_CODE"              => $arResult["VARIABLES"]["SECTION_CODE"],
-                "IBLOCK_URL"                => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"],
-                "SEARCH_PAGE"               => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["search"],
-                "USE_SHARE"                 => $arParams["USE_SHARE"],
-                "SHARE_HIDE"                => $arParams["SHARE_HIDE"],
-                "SHARE_TEMPLATE"            => $arParams["SHARE_TEMPLATE"],
-                "SHARE_HANDLERS"            => $arParams["SHARE_HANDLERS"],
-                "SHARE_SHORTEN_URL_LOGIN"   => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-                "SHARE_SHORTEN_URL_KEY"     => $arParams["SHARE_SHORTEN_URL_KEY"],
-                "ADD_ELEMENT_CHAIN"         => (isset($arParams["ADD_ELEMENT_CHAIN"]) ? $arParams["ADD_ELEMENT_CHAIN"] : ''),
-                "USE_RATING"                => $arParams["USE_RATING"],
-                "MAX_VOTE"                  => $arParams["MAX_VOTE"],
-                "VOTE_NAMES"                => $arParams["VOTE_NAMES"],
-                "MEDIA_PROPERTY"            => $arParams["MEDIA_PROPERTY"],
-                "SLIDER_PROPERTY"           => $arParams["SLIDER_PROPERTY"],
-                "TEMPLATE_THEME"            => $arParams["TEMPLATE_THEME"],
-                "STRICT_SECTION_CHECK"      => $arParams["STRICT_SECTION_CHECK"],
+                "FILTER_NAME"               => "arCategoryFilter",
+                "CACHE_FILTER"              => "Y",
+                "DISPLAY_TOP_PAGER"         => "N",
+                "DISPLAY_BOTTOM_PAGER"      => "N",
             ),
             $component
-        );
-
-        if ( ! defined('DETAIL_ELEMENT_ID')) {
-            define('DETAIL_ELEMENT_ID', $ElementID);
-        }
-        ?>
+        ); ?>
+        <? endforeach ?>
     </section>
-
-    <? if ($arParams['BACKLINK_TEXT']): ?>
-        <div class="container news__backlink--wrap">
-            <p class="news__backlink">
-                <a class="btn btn-primary"
-                   href="<?= $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["news"] ?>"><?= $arParams['BACKLINK_TEXT'] ?></a>
-            </p>
-        </div>
-    <? endif ?>
-
-    <? if ($arParams["USE_CATEGORIES"] == "Y" && $ElementID): ?>
-        <section class="news__categories component--news-list">
-            <? // bx, why logic is here?
-            global $arCategoryFilter;
-            $obCache    = new CPHPCache;
-            $strCacheID = $componentPath . LANG . $arParams["IBLOCK_ID"] . $ElementID . $arParams["CATEGORY_CODE"];
-            if (($tzOffset = CTimeZone::GetOffset()) <> 0) {
-                $strCacheID .= "_" . $tzOffset;
-            }
-            if ($arParams["CACHE_TYPE"] == "N" || $arParams["CACHE_TYPE"] == "A" && COption::GetOptionString("main",
-                    "component_cache_on", "Y") == "N") {
-                $CACHE_TIME = 0;
-            } else {
-                $CACHE_TIME = $arParams["CACHE_TIME"];
-            }
-            if ($obCache->StartDataCache($CACHE_TIME, $strCacheID, $componentPath)) {
-                $rsProperties     = CIBlockElement::GetProperty($arParams["IBLOCK_ID"], $ElementID, "sort", "asc",
-                    array("ACTIVE" => "Y", "CODE" => $arParams["CATEGORY_CODE"]));
-                $arCategoryFilter = array();
-                while ($arProperty = $rsProperties->Fetch()) {
-                    if (is_array($arProperty["VALUE"]) && count($arProperty["VALUE"]) > 0) {
-                        foreach ($arProperty["VALUE"] as $value) {
-                            $arCategoryFilter[$value] = true;
-                        }
-                    } elseif ( ! is_array($arProperty["VALUE"]) && strlen($arProperty["VALUE"]) > 0) {
-                        $arCategoryFilter[$arProperty["VALUE"]] = true;
-                    }
-                }
-                $obCache->EndDataCache($arCategoryFilter);
-            } else {
-                $arCategoryFilter = $obCache->GetVars();
-            }
-            if (count($arCategoryFilter) > 0):
-                $arCategoryFilter = array(
-                    "PROPERTY_" . $arParams["CATEGORY_CODE"] => array_keys($arCategoryFilter),
-                    "!" . "ID"                               => $ElementID,
-                );
-                ?>
-                <hr/><h3><?= GetMessage("CATEGORIES") ?></h3>
-                <? foreach ($arParams["CATEGORY_IBLOCK"] as $iblock_id): ?>
-                <? $APPLICATION->IncludeComponent(
-                    "bitrix:news.list",
-                    $arParams["LIST_TEMPLATE"],
-                    Array(
-                        "IBLOCK_ID"                 => $iblock_id,
-                        "NEWS_COUNT"                => $arParams["CATEGORY_ITEMS_COUNT"],
-                        "SET_TITLE"                 => "N",
-                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-                        "CACHE_TYPE"                => $arParams["CACHE_TYPE"],
-                        "CACHE_TIME"                => $arParams["CACHE_TIME"],
-                        "CACHE_GROUPS"              => $arParams["CACHE_GROUPS"],
-                        "FILTER_NAME"               => "arCategoryFilter",
-                        "CACHE_FILTER"              => "Y",
-                        "DISPLAY_TOP_PAGER"         => "N",
-                        "DISPLAY_BOTTOM_PAGER"      => "N",
-                    ),
-                    $component
-                ); ?>
-            <? endforeach ?>
-            <? endif ?>
-        </section>
     <? endif ?>
 
     <? if ($arParams["USE_REVIEW"] == "Y" && IsModuleInstalled("forum") && $ElementID): ?>
-        <section class="news__reviews component--forum-topic-reviews">
-            <hr/>
-            <? $APPLICATION->IncludeComponent(
-                "bitrix:forum.topic.reviews",
-                "",
-                Array(
-                    "CACHE_TYPE"           => $arParams["CACHE_TYPE"],
-                    "CACHE_TIME"           => $arParams["CACHE_TIME"],
-                    "MESSAGES_PER_PAGE"    => $arParams["MESSAGES_PER_PAGE"],
-                    "USE_CAPTCHA"          => $arParams["USE_CAPTCHA"],
-                    "PATH_TO_SMILE"        => $arParams["PATH_TO_SMILE"],
-                    "FORUM_ID"             => $arParams["FORUM_ID"],
-                    "URL_TEMPLATES_READ"   => $arParams["URL_TEMPLATES_READ"],
-                    "SHOW_LINK_TO_FORUM"   => $arParams["SHOW_LINK_TO_FORUM"],
-                    "DATE_TIME_FORMAT"     => $arParams["DETAIL_ACTIVE_DATE_FORMAT"],
-                    "ELEMENT_ID"           => $ElementID,
-                    "AJAX_POST"            => $arParams["REVIEW_AJAX_POST"],
-                    "IBLOCK_ID"            => $arParams["IBLOCK_ID"],
-                    "URL_TEMPLATES_DETAIL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
-                ),
-                $component
-            ); ?>
-        </section>
+    <section class="news__reviews component--forum-topic-reviews">
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:forum.topic.reviews",
+            "",
+            array_merge($arParams, array(
+                "ELEMENT_ID"           => $ElementID,
+                "URL_TEMPLATES_DETAIL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+            )),
+            $component
+        ); ?>
+    </section>
     <? endif ?>
 </section><!-- .component--news -->
